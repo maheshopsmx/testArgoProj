751bf098d95fcf854c7da7ba7cf52b09dc7d4488
gopaljayanythi <gopal.jayanti#opsmx.io>
2022-11-19 20:18:54 +0530

    changed start.sh

--- bg-template/start.sh
+++ bg-template/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir -p opsmx
-cd opsmx
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-template/create.sh -o create.sh
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-template/rollout.yaml -o rollout.yaml
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-template/service.yaml -o service.yaml
@@ -10,5 +8,5 @@ curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-templa
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-template/secret.yaml -o secret.yaml
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-template/instructions.txt -o instructions.txt
 
-echo Please read instructions.txt in this folder before running create.sh with required arguments
+echo Please read instructions.txt in folder before running create.sh with required arguments
 
