1412eb3c10e81c26ee6ea522c1520f7811c736c0
gopaljayanthi <56107926+gopaljayanthi@users.noreply.github.com>
2023-01-05 22:07:16 +0530

    Create download.sh

---
+++ bg-411-sns-templates/download.sh
@@ -0,0 +1,8 @@
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/rollout.tmpl -o rollout.tmpl
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/analysistemplate.tmpl -o analysistemplate.tmpl
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/configmap.tmpl -o configmap.tmpl
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/metrixtemplate.tmpl -o metrixtemplate.tmpl
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/opsmx-profile-secret.tmpl -o opsmx-profile-secret.tmpl
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/sa-role-rb.tmpl  -o sa-role-rb.tmpl
+curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/setup.sh -o setup.sh
+chmod +x./setup.sh
