1ba4cfb13aa0ae9006011a435c88e7f593e5c21d
gopaljayanthi <56107926+gopaljayanthi@users.noreply.github.com>
2023-01-05 22:22:03 +0530

    Update download.sh

--- bg-411-sns-templates/download.sh
+++ bg-411-sns-templates/download.sh
@@ -5,4 +5,4 @@ curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sn
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/opsmx-profile-secret.tmpl -o opsmx-profile-secret.tmpl
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/sa-role-rb.tmpl  -o sa-role-rb.tmpl
 curl https://raw.githubusercontent.com/gopaljayanthi/testArgoProj/main/bg-411-sns-templates/setup.sh -o setup.sh
-chmod +x./setup.sh
+chmod +x ./setup.sh
